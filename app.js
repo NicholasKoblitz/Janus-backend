7c55bdd4be2f90290154dce7d526db2f266eff12
NicholasK <nicholask1@outlook.com>
2022-07-29 10:13:06 -0400

    added app.options('*', cors())

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(authenticateJWT);
+app.options('*', cors())
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", usersRoutes);
