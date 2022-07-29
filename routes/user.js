fc37c56f003edbd48b09697a0678384dbe96f517
NicholasK <nicholask1@outlook.com>
2022-07-29 11:25:43 -0400

    Updated Route

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 /** Routes for users. */
 const jsonschema = require("jsonschema");
 const express = require("express");
-const { ensureCorrectUserOrTeacher, ensureTeacher } = require("../middleware/auth");
+const { ensureCorrectUserOrTeacher, ensureTeacher, ensureLoggedIn } = require("../middleware/auth");
 const assignUserSchema = require('../schemas/assignUserSchema.json');
 const { BadRequestError } = require("../expressError");
 const User = require("../models/user");
@@ -20,7 +20,7 @@ router.get('/:username/courses', ensureCorrectUserOrTeacher,async function(req, 
     }
 });
 
-router.post('/assign', ensureCorrectUserOrTeacher, async function(req, res, next) {
+router.post('/assign', ensureLoggedIn, async function(req, res, next) {
     try{
         const validator = jsonschema.validate(req.body, assignUserSchema);
         if(!validator.valid) {
