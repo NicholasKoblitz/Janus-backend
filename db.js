9891a05e324f438c61bb96bf5c6eaf20cc0d9b4e
NicholasKoblitz <nicholask1@outlook.com>
2024-12-19 15:10:29 -0500

    Changed database connection

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const {Client} = require('pg');
 
 let db = new Client({
-    connectionString: "DATABASE_URL=postgresql://postgres.pmbpgyeoydjkhwqgxbay:hqTKe0v2uX32h5gg@aws-0-us-east-1.pooler.supabase.com:6543/postgres"
+    connectionString: "postgresql://postgres.pmbpgyeoydjkhwqgxbay:hqTKe0v2uX32h5gg@aws-0-us-east-1.pooler.supabase.com:6543/postgres"
 })
 
 db.connect();
